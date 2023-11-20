c05e63693ac74b38ed37172a04a6d3e9d1dabdfd
jstamandtech <125502250+jstamandtech@users.noreply.github.com>
2023-11-20 08:38:39 -0500

    Update Main.cu

--- src/Main.cu
+++ src/Main.cu
@@ -1,12 +1,4 @@
-/* *
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "Manager.h"
@@ -15,6 +7,6 @@
  * Host function that prepares data array and passes it to the CUDA kernel.
  */
 int main(void) {
-
+	// replace with own matrices to solve
 	Manager man("../../datasets/wine/X.mat", "X", "../../datasets/wine/Y.mat", "Y");
 }
